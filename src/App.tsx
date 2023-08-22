76945648accae91163f401ffd0811078498fb2b3
Leonardo Pulpor <pulppor@hotmail.com>
2023-08-22 15:48:42 -0300

    req 7 + req 10 ok!

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './components/Login';
 import TesteRequired from './components/TesteRequired';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/meals" element={ <TesteRequired /> } />
+        <Route
+          path="/teste-required"
+          element={ <TesteRequired /> }
+        />
+        <Route path="/search" element={ <SearchBar /> } />
       </Routes>
     </BrowserRouter>
   );
