f2209d52920876c5dff23e2c37a39d5952b28366
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-08-22 20:00:27 -0300

    passando final do requisito devolta ao Leo

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -4,40 +4,40 @@ import { Meal, Drink } from './types';
 export async function searchRecipesByIngredient(ingredient: string): Promise<Meal[]> {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const data = await response.json();
-  return data.meals || [];
+  return data.meals;
 }
 
 // Função para buscar receitas por nome
 export async function searchRecipesByName(name: string): Promise<Meal[]> {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
   const data = await response.json();
-  return data.meals || [];
+  return data.meals;
 }
 
 // Função para buscar receitas por primeira letra
 export async function searchRecipesByFirstLetter(letter: string): Promise<Meal[]> {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
   const data = await response.json();
-  return data.meals || [];
+  return data.meals;
 }
 
 // Função para buscar Drinks por ingrediente
 export async function searchDrinksByIngredient(ingredient: string): Promise<Drink[]> {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const data = await response.json();
-  return data.drinks || [];
+  return data.drinks;
 }
 
 // Função para buscar Drinks por nome
 export async function searchDrinksByName(name: string): Promise<Drink[]> {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
   const data = await response.json();
-  return data.drinks || [];
+  return data.drinks;
 }
 
 // Função para buscar Drinks por primeira letra
 export async function searchDrinksByFirstLetter(letter: string): Promise<Drink[]> {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
   const data = await response.json();
-  return data.drinks || [];
+  return data.drinks;
 }
