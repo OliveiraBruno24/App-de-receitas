082272f23ceab07fb1acc13859175c93ee5ec633
Stanley Magno Alves Brandao <brandaostanley0@gmail.com>
2023-08-25 17:42:21 -0300

    req 60-61-62 e testes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
-  const storedEmail = localStorage.getItem('user');
+  const navigate = useNavigate();
+  const storedEmail = localStorage.getItem('user'); // Se o e-mail armazenado em localStorage está visível.
+
+  const handleDoneRecipesClick = () => {
+    navigate('/done-recipes'); // Ao clicar no botão de `Done Recipes, a rota mude para a tela de receitas feitas.
+  };
+
+  const handleFavoriteRecipesClick = () => {
+    navigate('/favorite-recipes'); // Ao clicar no botão de Favorite Recipes, a rota mude para a tela de receitas favoritas.
+  };
+
+  const handleLogoutClick = () => {
+    localStorage.clear(); // Limpa todas as chaves do localStorage.
+    navigate('/'); // Redireciona para a tela de login.
+  };
 
   return (
     <div>
@@ -13,9 +27,15 @@ function Profile() {
           <span data-testid="profile-email">{storedEmail}</span>
         </p>
       </div>
-      <button data-testid="profile-done-btn">Done Recipes</button>
-      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button data-testid="profile-logout-btn">Logout</button>
+      <button data-testid="profile-done-btn" onClick={ handleDoneRecipesClick }>
+        Done Recipes
+      </button>
+      <button data-testid="profile-favorite-btn" onClick={ handleFavoriteRecipesClick }>
+        Favorite Recipes
+      </button>
+      <button data-testid="profile-logout-btn" onClick={ handleLogoutClick }>
+        Logout
+      </button>
     </div>
   );
 }
