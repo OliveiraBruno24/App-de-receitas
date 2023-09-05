80e1d18d16c61c076de198d5504b328efb1ed60f
Leonardo Pulpor <pulppor@hotmail.com>
2023-09-05 13:40:47 -0300

    teste

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -2,8 +2,8 @@
 // import { MemoryRouter } from 'react-router-dom';
 // import SearchBar from './Header/SearchBar';
 
-describe('Componente SearchBar', () => {
-  test('', () => {
-
+describe('Componente searchbar', () => {
+  it('teste o searchbar', () => {
+    expect(true).toBe(true);
   });
 });
